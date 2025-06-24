0387ffd2be26bf291c6e1c795f85192de79d9dc7
Dylan <186506596+crab159@users.noreply.github.com>
2025-06-24 08:44:09 -0400

    Add bet management functionality

--- autoload/BetManager.gd
+++ autoload/BetManager.gd
@@ -1,1 +1,24 @@
 extends Node
+
+
+var bet_pool: Array [Bet]
+var winnings: int = 0
+
+
+# TODO decide if bet pool is even necessary
+func take_bet(player: Player, bet_amount: int) -> void:
+	if bet_amount > player.chips:
+		bet_amount = min(bet_amount, player.chips)
+	if player.chips >= bet_amount:
+		player.chips -= bet_amount
+		var player_bet: Bet = Bet.new(player, bet_amount)
+		bet_pool.append(player_bet)
+	winnings += bet_amount
+
+
+func divide_winnings(winners: Array) -> void:
+	var number_of_winners: int = winners.size()
+	var winnings_amount: int = winnings % number_of_winners
+	winnings -= winnings_amount
+	for winner in winners:
+		winner.chips += winnings
