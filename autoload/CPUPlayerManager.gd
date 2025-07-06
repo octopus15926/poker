39c0b91b5bff6f020dfe528794e687a57aad302c
Dylan <186506596+octopus15926@users.noreply.github.com>
2025-07-06 18:15:26 -0400

    Add debug text

--- autoload/CPUPlayerManager.gd
+++ autoload/CPUPlayerManager.gd
@@ -2,6 +2,6 @@ extends Node
 
 
 func make_bet(cpu_player: Player) -> void:
-	print(cpu_player.player_name + &" makes a bet!")
+	print(cpu_player.player_name + &" makes a bet of " + str(floor(cpu_player.chips/4.0)) + &"!")
 	BetManager.take_bet(cpu_player, floor(cpu_player.chips/4.0))
 	cpu_player.finish_turn()
